d3dcba5582d86256596b37499e98c1199bbc80a6
Manoj <104271094+Manojachari@users.noreply.github.com>
2024-01-09 00:42:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+const navItems = document.querySelectorAll('.nav-item a');
+
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navItems.forEach(link => link.classList.remove('active'));
+        item.classList.add('active');
+    });
+});
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+});
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+
+
+//text animation
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector(".bg-heading").onmouseover = event => {  
+  let iteration = 0;
+  
+  clearInterval(interval);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 26)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 3;
+  }, 30);
+}
+
